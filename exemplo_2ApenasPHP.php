2c850ee53b5748c1f635f4adc61bf44cf7aa08f2
TiagoUniverse <tiagocesar68@gmail.com>
2023-06-01 17:24:09 -0300

    Criando a paginação apenas com PHP

--- exemplo_2ApenasPHP.php
+++ exemplo_2ApenasPHP.php
@@ -1,49 +1,55 @@
 <?php
 // Data: 31/05/23
 
-// Conexão ao BD
-// $con = mysqli_connect('localhost' , 'root' , '');
-// mysqli_select_db($con, 'padaria');
-
-
-$servername = "localhost";
-$dbname = "controle-financeiro";
-$username = "root";
-$pwd = "";
-
-try{
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname" , "$username" , "$pwd");
-    // echo "Conectado com sucesso";
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch (Exception $e){
-    die(print_r($e->getMessage()));
+if (!isset($_GET['pagina'])){
+    $pagina = 1;
+} else{
+    $pagina = filter_input(INPUT_GET, "pagina", FILTER_VALIDATE_INT);
 }
-
-
- $result = $pdo->query("Select * from status_mes");
-
- while($linha = $result->fetch(\PDO::FETCH_ASSOC)){
-    echo $linha['nome'] . "<br>";
- }
-
-$quantidadeResultado = $result->rowCount();
-
 // Defina quantos resultados quer por página
-$limite = 10;
+
+$limite = 2;
+$inicio = ($pagina - 1) * $limite;
+
+$pdo = new PDO("mysql:host=localhost;dbname=padaria", "root", "");
+$stmt = $pdo->query("Select nome,preco from comida LIMIT $inicio,$limite")->fetchAll();
+
+$QuantidadeRegistros = $pdo->query(" Select COUNT(*) as 'total' from comida   ")->fetch()['total'];
+
+$UltimaPagina =  ceil($QuantidadeRegistros / $limite);
+
+// echo count($stmt);
+// var_dump($stmt);
 
 
-//  
+
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paginação com PHP</title>
 </head>
+
 <body>
-    
+    <ul>
+        <?php foreach ($stmt as $item) : ?>
+            <li> <?= $item['nome']; ?> </li>
+        <?php endforeach ?>
+    </ul>
+
+    <a href="?pagina=1">Primeira</a>
+    <a href="?pagina=<?= $pagina - 1 ?>"><<</a>
+    <?=$pagina?>
+    <a href="?pagina=<?= $pagina + 1 ?>">>></a>
+    <a href="?pagina= <?= $UltimaPagina ?>  ">Última</a>
+
+
+
 </body>
+
 </html>
