b7a2fa9397c43d3bb300f18bab6b0e3d43d23946
TiagoUniverse <tiagocesar68@gmail.com>
2023-06-01 17:34:02 -0300

    Validação aplicada na navegação da paginação com sucesso!

--- exemplo_2ApenasPHP.php
+++ exemplo_2ApenasPHP.php
@@ -1,27 +1,32 @@
 <?php
 // Data: 31/05/23
 
-if (!isset($_GET['pagina'])){
-    $pagina = 1;
-} else{
-    $pagina = filter_input(INPUT_GET, "pagina", FILTER_VALIDATE_INT);
-}
-// Defina quantos resultados quer por página
-
-$limite = 2;
-$inicio = ($pagina - 1) * $limite;
 
 $pdo = new PDO("mysql:host=localhost;dbname=padaria", "root", "");
-$stmt = $pdo->query("Select nome,preco from comida LIMIT $inicio,$limite")->fetchAll();
+
+// Limite de itens sendo exibidos na página
+$limite = 2;
 
 $QuantidadeRegistros = $pdo->query(" Select COUNT(*) as 'total' from comida   ")->fetch()['total'];
 
 $UltimaPagina =  ceil($QuantidadeRegistros / $limite);
 
+// Variáveis
+if (!isset($_GET['pagina']) || $_GET['pagina'] > $UltimaPagina ){
+    $pagina = 1;
+} else{
+    $pagina = filter_input(INPUT_GET, "pagina", FILTER_VALIDATE_INT);
+}
+
+
+$inicio = ($pagina - 1) * $limite;
+
+// Consulta SQL
+$stmt = $pdo->query("Select nome,preco from comida LIMIT $inicio,$limite")->fetchAll();
+
+
 // echo count($stmt);
 // var_dump($stmt);
-
-
 
 ?>
 
@@ -43,9 +48,19 @@ $UltimaPagina =  ceil($QuantidadeRegistros / $limite);
     </ul>
 
     <a href="?pagina=1">Primeira</a>
+
+    <?php if($pagina >1): ?>
     <a href="?pagina=<?= $pagina - 1 ?>"><<</a>
+    <?php endif ?> 
+
+
     <?=$pagina?>
+
+
+    <?php if ($pagina<$UltimaPagina): ?>
     <a href="?pagina=<?= $pagina + 1 ?>">>></a>
+    <?php endif ?>
+
     <a href="?pagina= <?= $UltimaPagina ?>  ">Última</a>
 
 
