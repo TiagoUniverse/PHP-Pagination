1faee2102eefc6326cade97a87ac1f2f9d62f395
TiagoUniverse <tiagocesar68@gmail.com>
2023-06-01 15:48:17 -0300

    Estudando como usar os dados do BD

--- dados_pagina.php
+++ dados_pagina.php
@@ -9,15 +9,20 @@ if (isset($_GET['pagina'])) {
 
     // Aqui você pode realizar qualquer lógica necessária para obter os dados correspondentes à página solicitada
     // Por exemplo, realizar uma consulta ao banco de dados
+    $pdo = new PDO("mysql:host=localhost;dbname=padaria" , "root" , "");
+    $stmt = $pdo->query("Select * from comida ")->fetchAll();
+    // var_dump($stmt);
     
+    // echo $stmt[0][1];
+
     // Supondo que você tenha uma array de dados que serão exibidos na páginação
-    $dados = array(1 , 2 , 3 , 4 , 5 , 6);
+    $dados = array('Tiago Cesar' , 'papi', 'camila' , 'ivone' , 'graça' , 'diego');
     
     // Calcula o índice de início da página com base no número da página e no número de itens por página
     $indiceInicio = ($pagina - 1) * $itensPorPagina;
     
     // Filtra os dados com base no índice de início e no número de itens por página
-    $dadosPagina = array_slice($dados, $indiceInicio, $itensPorPagina);
+    $dadosPagina = array_slice($stmt, $indiceInicio, $itensPorPagina);
     
     // Retorna os dados da página no formato JSON
     echo json_encode($dadosPagina);
